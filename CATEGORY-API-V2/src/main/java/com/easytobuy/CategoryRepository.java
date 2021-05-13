8db8e5af0cca0e8d94069627a2d7713328217374
MaruthamSatish <smarutham@gmail.com>
2021-05-13 11:24:27 +0530

    Category-API Rest Services

--- CATEGORY-API-V2/src/main/java/com/easytobuy/CategoryRepository.java
+++ CATEGORY-API-V2/src/main/java/com/easytobuy/CategoryRepository.java
@@ -3,16 +3,16 @@
  */
 package com.easytobuy;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author SatishReddy
  *
  */
-
+@Repository
 public interface CategoryRepository  extends JpaRepository<Category,Long>{
 	//sList<Category> findByCategoryName(String categoryName);
 	Optional<Category> findByCategoryId(Integer categoryId);
