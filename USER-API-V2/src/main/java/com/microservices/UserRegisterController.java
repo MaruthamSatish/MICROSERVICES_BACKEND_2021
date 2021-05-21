251573223d7e936cf1913fda76bb771c9e448c35
MaruthamSatish <smarutham@gmail.com>
2021-05-21 09:44:01 +0530

    Config Server

--- USER-API-V2/src/main/java/com/microservices/UserRegisterController.java
+++ USER-API-V2/src/main/java/com/microservices/UserRegisterController.java
@@ -1,9 +1,12 @@
 package com.microservices;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/userRegister")
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/user")
+
+
 public class UserRegisterController {
 @Autowired
 public UserRepository userRepository;
-	@PostMapping
-	
+
+@Value("${config.value}")
+private String configValue;
+
+	@PostMapping("/signUp")
 	public ResponseEntity<User> userSignUp(@RequestBody User user) {
 		User userDetails = userRepository.insert(user);
 		return new ResponseEntity<User>(userDetails,HttpStatus.CREATED);
-		
-		
+	}
+	@GetMapping("/signIn")
+	public Optional<User> userSignIn(@RequestBody User user) {
+    Optional<User> userDetails = userRepository.findByUserNameAndUserPassword(user.getUserName(),user.getUserPassword());
+    
+		return userDetails;
+	}
+	@GetMapping("/configServer")
+	public String testConfigServer() {
+		return configValue;
 	}
 }
